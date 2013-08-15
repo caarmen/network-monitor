bce63b519ffd73d67c297daf9f32e902d6dc3a2f
Carmen Alvarez <c@rmen.ca>
2013-08-16 00:15:15 +0200

    Removing special character from comment

--- src/org/jraf/android/networkmonitor/app/export/ExcelExport.java
+++ src/org/jraf/android/networkmonitor/app/export/ExcelExport.java
@@ -141,7 +141,7 @@ public class ExcelExport extends FileExport {
 
         // "integer that specifies the column width in units of 1/256th of a character width. 
         // Character width is defined as the maximum digit width of the numbers 0, 1, 2, ... 9 
-        // as rendered in the Normal style’s font."
+        // as rendered in the Normal style's font."
 
         // Some of our letters may be wider than the digits 0-9. So we may need to overestimate
         // the width we need by a bit. Adding a padding of 4 characters seems to be ok
