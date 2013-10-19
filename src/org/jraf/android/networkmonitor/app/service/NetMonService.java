0044dcb5c030f68d62a5ed56515a918b411344aa
Carmen Alvarez <c@rmen.ca>
2013-10-19 15:16:09 +0200

    Issue #14: Only log the WiFi SSID and WiFi signal strength if we are currently connected to a WiFi network.

--- src/org/jraf/android/networkmonitor/app/service/NetMonService.java
+++ src/org/jraf/android/networkmonitor/app/service/NetMonService.java
@@ -509,7 +509,7 @@ public class NetMonService extends Service {
     private ContentValues getWifiInfo() {
         WifiInfo connectionInfo = mWifiManager.getConnectionInfo();
         ContentValues result = new ContentValues(2);
-        if (connectionInfo == null) return result;
+        if (connectionInfo == null || connectionInfo.getNetworkId() < 0) return result;
         result.put(NetMonColumns.WIFI_SSID, connectionInfo.getSSID());
         int signalLevel = WifiManager.calculateSignalLevel(connectionInfo.getRssi(), 5);
         result.put(NetMonColumns.WIFI_SIGNAL_STRENGTH, signalLevel);
