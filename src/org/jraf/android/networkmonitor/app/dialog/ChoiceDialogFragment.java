9c652d52fe3ec3f59a9f28417126ed4a82e42fb8
Carmen Alvarez <c@rmen.ca>
2014-03-23 05:56:23 +0100

    Changed license of ChoiceDialogFragment

--- src/org/jraf/android/networkmonitor/app/dialog/ChoiceDialogFragment.java
+++ src/org/jraf/android/networkmonitor/app/dialog/ChoiceDialogFragment.java
@@ -1,20 +1,25 @@
-/**
- * Copyright 2013 Carmen Alvarez
+/*
+ * This source is part of the
+ *      _____  ___   ____
+ *  __ / / _ \/ _ | / __/___  _______ _
+ * / // / , _/ __ |/ _/_/ _ \/ __/ _ `/
+ * \___/_/|_/_/ |_/_/ (_)___/_/  \_, /
+ *                              /___/
+ * repository.
  *
- * This file is part of Scrum Chatter.
+ * Copyright (C) 2014 Carmen Alvarez (c@rmen.ca)
  *
- * Scrum Chatter is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Scrum Chatter is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License
- * along with Scrum Chatter. If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jraf.android.networkmonitor.app.dialog;
 
