6316782e5c45dee8bb0051032851c502d5101c68
Carmen Alvarez <c@rmen.ca>
2014-06-28 17:11:54 +0200

    Issue #43: Add a help screen for speed test.

---
+++ src/ca/rmen/android/networkmonitor/app/speedtest/SpeedTestAboutFragment.java
@@ -0,0 +1,54 @@
+/*
+ * This source is part of the
+ *      _____  ___   ____
+ *  __ / / _ \/ _ | / __/___  _______ _
+ * / // / , _/ __ |/ _/_/ _ \/ __/ _ `/
+ * \___/_/|_/_/ |_/_/ (_)___/_/  \_, /
+ *                              /___/
+ * repository.
+ *
+ * Copyright (C) 2014 Carmen Alvarez (c@rmen.ca)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ca.rmen.android.networkmonitor.app.speedtest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import ca.rmen.android.networkmonitor.Constants;
+import ca.rmen.android.networkmonitor.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SpeedTestAboutFragment extends Fragment {
+    private static final String TAG = Constants.TAG + SpeedTestAboutFragment.class.getSimpleName();
+
+    public SpeedTestAboutFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_speed_test_about, container, false);
+        WebView wvCredits = (WebView) rootView.findViewById(R.id.webview);
+        wvCredits.setBackgroundColor(0);
+        wvCredits.getSettings().setDefaultFontSize(14);
+        wvCredits.loadUrl("file:///android_asset/speed_test_about.html");
+        return rootView;
+    }
+
+}
