e3b7332af71b017d0a29e3802fd66e7b151a6019
Carmen Alvarez <c@rmen.ca>
2014-10-25 19:52:29 +0200

    Fix tag for BootReceiver logs

--- src/ca/rmen/android/networkmonitor/app/service/BootReceiver.java
+++ src/ca/rmen/android/networkmonitor/app/service/BootReceiver.java
@@ -27,11 +27,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import ca.rmen.android.networkmonitor.Constants;
 import ca.rmen.android.networkmonitor.app.prefs.NetMonPreferences;
 import ca.rmen.android.networkmonitor.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
-    private static final String TAG = BootReceiver.class.getSimpleName();
+    private static final String TAG = Constants.TAG + "/" + BootReceiver.class.getSimpleName();
 
     @Override
     public void onReceive(Context context, Intent intent) {
