715a8230f8042877b61aeffd9276eb673b24422a
Carmen Alvarez <c@rmen.ca>
2015-10-11 17:53:56 +0200

    Prevent crashing when opening the multiselectlist peference

--- networkmonitor/src/main/java/ca/rmen/android/networkmonitor/app/prefs/NetMonPreferenceFragment.java
+++ networkmonitor/src/main/java/ca/rmen/android/networkmonitor/app/prefs/NetMonPreferenceFragment.java
@@ -23,13 +23,19 @@
  */
 package ca.rmen.android.networkmonitor.app.prefs;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v14.preference.MultiSelectListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
+
+import ca.rmen.android.networkmonitor.app.dialog.MultiSelectListPreferenceDialogFragment;
 
 
 public class NetMonPreferenceFragment extends PreferenceFragmentCompat {
 
+    private static final String FRAGMENT_TAG_DIALOG = "android.support.v7.preference.PreferenceFragment.DIALOG";
     private static final String EXTRA_PREFERENCE_FILE_RES_ID = NetMonPreferenceFragment.class.getPackage().getName() + "_preference_file_res_id";
 
     public static NetMonPreferenceFragment newInstance(int preferenceFileResId) {
@@ -47,4 +53,18 @@ public class NetMonPreferenceFragment extends PreferenceFragmentCompat {
         PreferenceManager.setDefaultValues(getActivity(), preferenceFileResId, false);
     }
 
+    @Override
+    public void onDisplayPreferenceDialog(Preference preference) {
+        // Hack to allow a MultiSelectListPreference
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH
+                && preference instanceof MultiSelectListPreference) {
+            if (getFragmentManager().findFragmentByTag(FRAGMENT_TAG_DIALOG) == null) {
+                MultiSelectListPreferenceDialogFragment dialogFragment = MultiSelectListPreferenceDialogFragment.newInstance(preference.getKey());
+                dialogFragment.setTargetFragment(this, 0);
+                dialogFragment.show(getFragmentManager(), FRAGMENT_TAG_DIALOG);
+            }
+        } else {
+            super.onDisplayPreferenceDialog(preference);
+        }
+    }
 }
