d072da303621ffcbb7ac0ec4b42e23bbe5a01382
Carmen Alvarez <c@rmen.ca>
2019-02-24 15:14:34 +0100

    Remove StandardCharsets.UTF_8 (not available on api level 14)

--- networkmonitor/src/main/java/ca/rmen/android/networkmonitor/app/service/datasources/ConnectionTesterDataSource.java
+++ networkmonitor/src/main/java/ca/rmen/android/networkmonitor/app/service/datasources/ConnectionTesterDataSource.java
@@ -42,7 +42,6 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
 
 import ca.rmen.android.networkmonitor.Constants;
 import ca.rmen.android.networkmonitor.app.prefs.NetMonPreferences;
@@ -155,7 +154,7 @@ public class ConnectionTesterDataSource implements NetMonDataSource {
 
             Log.d(TAG, "getSocketTestResult Sending GET...");
             OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(HTTP_GET.getBytes(StandardCharsets.UTF_8));
+            outputStream.write(HTTP_GET.getBytes("utf-8"));
             outputStream.flush();
             Log.d(TAG, "getSocketTestResult Sent GET");
             InputStream inputStream = socket.getInputStream();
